d086367d6036fcdea6f687b208f2ac240551be50
Miles Cole <milescole@Miless-MacBook-Pro.local>
2018-11-27 10:50:37 -0800

    Server up and running

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
