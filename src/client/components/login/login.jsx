9c043fb8b7cab49978011371ebaa3947d097e26f
Garron Ma <garronma@gmail.com>
2018-11-29 14:10:22 -0800

    revise login/signup for resizing

--- src/client/components/login/login.jsx
+++ src/client/components/login/login.jsx
@@ -79,29 +79,33 @@ class Login extends React.Component {
 
   render() {
     return (
-      <div className='login'>
-        <div className='login-top'>
-          <p className='login-top-title'>BITES</p>
-          <p className='login-top-descrip'>Inventory Management System for Perishable Goods</p>
-        </div>
-        <div className='login-body'>
-          <form id='login-form' onSubmit={this.handleLogin}>
-            <label>Email:</label><br />
-            <input type='text' value={this.state.userEmail} onChange={this.updateEmail} />
-            {this.state.submitted &&
-              !this.state.userEmail &&
-              <div className='help-block'>Username is required</div>
-            }<br />
-            <label>Password:</label><br />
-            <input type='password' value={this.state.userPassword} onChange={this.updatePassword} />
-            {this.state.submitted &&
-              !this.state.userEmail &&
-              <div className='help-block'>Password is required</div>
-            }<br />
-            <input type='checkbox' value={this.state.rememberMe} onChange={this.updateRememberMe} /> Remember Me <br />
-            <input id='login-submit' type='submit' value='Login' /><br />
-          </form>
-          <Link to='/register'>Sign Up For An Account</Link>
+      <div className='login-box'>
+        <div className='login-content-box'>
+          <div className='login-top'>
+            <p className='login-top-title'>BITES</p>
+            <p className='login-top-descrip'>Inventory Management System for Perishable Goods</p>
+          </div>
+          <div className='login-body'>
+            <form id='login-form' onSubmit={this.handleLogin}>
+              <label>Email:</label><br />
+              <input type='text' value={this.state.userEmail} onChange={this.updateEmail} />
+              {this.state.submitted &&
+                !this.state.userEmail &&
+                <div className='help-block'>Username is required</div>
+              }<br />
+              <label>Password:</label><br />
+              <input type='password' value={this.state.userPassword} onChange={this.updatePassword} />
+              {this.state.submitted &&
+                !this.state.userEmail &&
+                <div className='help-block'>Password is required</div>
+              }<br />
+              <input type='checkbox' value={this.state.rememberMe} onChange={this.updateRememberMe} /> Remember Me <br />
+              <input id='login-submit' type='submit' value='Login' /><br />
+            </form>
+            <div className='login-footer'>
+              <Link to='/register'>Sign Up For An Account</Link>
+            </div>
+          </div>
         </div>
       </div>
     )
--- src/client/components/signup/signup.jsx
+++ src/client/components/signup/signup.jsx
@@ -131,69 +131,71 @@ class Register extends React.Component {
 
   render() {
     return (
-      <div className='register'>
-        <div className='register-top'>
-          <p className='register-top-title'>BITES</p>
-          <p className='register-top-descrip'>Inventory Management System for Perishable Goods</p>
-        </div>
-        <div className='register-body'>
-          <form id='register-form' onSubmit={this.handleRegister}>
-            <div className='register-body-left'>
-              <label>Company Name:</label><br />
-              <input type='text' value={this.state.companyName} onChange={this.updateCompanyName} placeholder='Enter your company name' />
-              {this.state.submitted &&
-                !this.state.companyName &&
-                <div className='help-block'>Company Name is required</div>
-              }<br />
-              <label>Email:</label><br />
-              <input type='text' value={this.state.userEmail} onChange={this.updateEmail} placeholder='Enter your email address' />
-              {this.state.submitted &&
-                !this.state.userEmail &&
-                <div className='help-block'>Email is required</div>
-              }<br />
-              <label>Password:</label><br />
-              <input type='password' value={this.state.userPassword} onChange={this.updatePassword} placeholder='Enter your password' />
-              {this.state.submitted &&
-                !this.state.userPassword &&
-                <div className='help-block'>Password is required</div>
-              }<br />
-              <label>Phone Number:</label><br />
-              <input type='text' value={this.state.userPhoneNumber} onChange={this.updatePhoneNumber} placeholder='(123) 456-7890' />
-              {this.state.submitted &&
-                !this.state.userPhoneNumber &&
-                <div className='help-block'>Phone Number is required</div>
-              }<br />
+      <div className='register-box'>
+        <div className='register-content-box'>
+          <div className='register-top'>
+            <p className='register-top-title'>BITES</p>
+            <p className='register-top-descrip'>Inventory Management System for Perishable Goods</p>
+          </div>
+          <div className='register-body'>
+            <form id='register-form' onSubmit={this.handleRegister}>
+              <div className='register-body-left'>
+                <label>Company Name:</label><br />
+                <input type='text' value={this.state.companyName} onChange={this.updateCompanyName} placeholder='Enter your company name' />
+                {this.state.submitted &&
+                  !this.state.companyName &&
+                  <div className='help-block'>Company Name is required</div>
+                }<br />
+                <label>Email:</label><br />
+                <input type='text' value={this.state.userEmail} onChange={this.updateEmail} placeholder='Enter your email address' />
+                {this.state.submitted &&
+                  !this.state.userEmail &&
+                  <div className='help-block'>Email is required</div>
+                }<br />
+                <label>Password:</label><br />
+                <input type='password' value={this.state.userPassword} onChange={this.updatePassword} placeholder='Enter your password' />
+                {this.state.submitted &&
+                  !this.state.userPassword &&
+                  <div className='help-block'>Password is required</div>
+                }<br />
+                <label>Phone Number:</label><br />
+                <input type='text' value={this.state.userPhoneNumber} onChange={this.updatePhoneNumber} placeholder='(123) 456-7890' />
+                {this.state.submitted &&
+                  !this.state.userPhoneNumber &&
+                  <div className='help-block'>Phone Number is required</div>
+                }<br />
+              </div>
+              <div className='register-body-right'>
+                <label>Street Address:</label><br />
+                <input type='text' value={this.state.userStreetAddress} onChange={this.updateStreetAddress} placeholder='1600 Main St.' />
+                {this.state.submitted &&
+                  !this.state.userStreetAddress &&
+                  <div className='help-block'>Street Address is required</div>
+                }<br />
+                <label>City:</label><br />
+                <input type='text' value={this.state.userCity} onChange={this.updateCity} placeholder='Venice' />
+                {this.state.submitted &&
+                  !this.state.userCity &&
+                  <div className='help-block'>City is required</div>
+                }<br />
+                <label>State:</label><br />
+                <input type='text' value={this.state.userState} onChange={this.updateState} placeholder='CA' />
+                {this.state.submitted &&
+                  !this.state.userState &&
+                  <div className='help-block'>State is required</div>
+                }<br />
+                <label>Zip Code:</label><br />
+                <input type='text' value={this.state.userZipCode} onChange={this.updateZipCode} placeholder='90210' />
+                {this.state.submitted &&
+                  !this.state.userZipCode &&
+                  <div className='help-block'>Zip Code is required</div>
+                }
+              </div>
+              <input id='register-submit' type='submit' value='Register' />
+            </form>
+            <div className='register-footer'>
+              <Link to='/'>Log In To Your Account</Link>
             </div>
-            <div className='register-body-right'>
-              <label>Street Address:</label><br />
-              <input type='text' value={this.state.userStreetAddress} onChange={this.updateStreetAddress} placeholder='1600 Main St.' />
-              {this.state.submitted &&
-                !this.state.userStreetAddress &&
-                <div className='help-block'>Street Address is required</div>
-              }<br />
-              <label>City:</label><br />
-              <input type='text' value={this.state.userCity} onChange={this.updateCity} placeholder='Venice' />
-              {this.state.submitted &&
-                !this.state.userCity &&
-                <div className='help-block'>City is required</div>
-              }<br />
-              <label>State:</label><br />
-              <input type='text' value={this.state.userState} onChange={this.updateState} placeholder='CA' />
-              {this.state.submitted &&
-                !this.state.userState &&
-                <div className='help-block'>State is required</div>
-              }<br />
-              <label>Zip Code:</label><br />
-              <input type='text' value={this.state.userZipCode} onChange={this.updateZipCode} placeholder='90210' />
-              {this.state.submitted &&
-                !this.state.userZipCode &&
-                <div className='help-block'>Zip Code is required</div>
-              }<br />
-            </div>
-            <input id='register-submit' type='submit' value='Register' /><br />
-          </form>
-          <div className='register-footer'>
-            <Link to='/'>Log In To Your Account</Link>
           </div>
         </div>
       </div>
