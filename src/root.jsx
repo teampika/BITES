fcd9bebbda2e93ec0db6ffdb24702504ea60b25d
Garron Ma <garronma@gmail.com>
2018-11-28 23:58:12 -0800

    add react router

--- src/root.jsx
+++ src/root.jsx
@@ -8,9 +8,18 @@
  */
 
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const root = () => (
-  <h1>What a beautiful day to code</h1>
+import Login from './client/components/login/login';
+import Register from './client/components/signup/signup'
+
+const Root = (props) => (
+  <Router>
+    <div>
+      <Route exact path='/' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </div>
+  </Router>
 )
 
-export default root;
+export default Root;
